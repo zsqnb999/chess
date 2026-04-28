180cf52db0964955911b6facab43406c1d86e4d8
zsqnb999 <zsqnb999@gmail.com>
2026-04-28 17:07:55 +0800

    Add files via upload

---
+++ chess2.cpp
@@ -0,0 +1,638 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+#include <chrono>
+using namespace std;
+
+static WORD Bg(WORD bg){ return (bg<<4); }
+static void SetAttr(WORD attr){ SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),attr); }
+static WORD GetDefaultAttr(){
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE),&csbi);
+	return csbi.wAttributes;
+}
+
+enum Color{ NONE=0,WHITE=1,BLACK=2 };
+
+struct Piece{ char t='.'; Color c=NONE; };
+static Piece Make(Color c,char t){ return Piece{t,c}; }
+
+struct Pos{ int r,c; };
+static bool In(int r,int c){ return r>=0&&r<8&&c>=0&&c<8; }
+
+struct CastlingRights{ bool wk=true,wq=true,bk=true,bq=true; };
+
+struct Move{
+	Pos from,to;
+	char promo=0;
+	bool isCastle=false;
+	bool isEnPassant=false;
+};
+
+struct Board{
+	Piece s[8][8];
+	Color sideToMove=WHITE;
+	CastlingRights cr;
+	bool epAvail=false;
+	Pos epTarget{-1,-1};
+};
+
+static bool SameMove(const Move&a,const Move&b){
+	return a.from.r==b.from.r&&a.from.c==b.from.c&&
+	a.to.r==b.to.r&&a.to.c==b.to.c&&
+	a.promo==b.promo&&a.isCastle==b.isCastle&&a.isEnPassant==b.isEnPassant;
+}
+
+static void Init(Board& b){
+	for(int r=0;r<8;r++) for(int c=0;c<8;c++) b.s[r][c]=Make(NONE,'.');
+	string back="RNBQKBNR";
+	for(int c=0;c<8;c++){
+		b.s[0][c]=Make(WHITE,back[c]);
+		b.s[1][c]=Make(WHITE,'P');
+		b.s[6][c]=Make(BLACK,'P');
+		b.s[7][c]=Make(BLACK,back[c]);
+	}
+	b.sideToMove=WHITE;
+	b.cr={true,true,true,true};
+	b.epAvail=false;
+	b.epTarget={-1,-1};
+}
+
+static void Draw(const Board& b,WORD defAttr,bool hasSel,Pos sel,const vector<Pos>& marks){
+	WORD light=Bg(7),dark=Bg(2);
+	WORD wfg=FOREGROUND_RED|FOREGROUND_GREEN|FOREGROUND_BLUE|FOREGROUND_INTENSITY;
+	WORD bfg=FOREGROUND_BLUE|FOREGROUND_INTENSITY;
+	WORD emptyFg=defAttr&0x0F;
+	
+	auto isMarked=[&](int r,int c){
+		for(auto &p: marks) if(p.r==r&&p.c==c) return true;
+		return false;
+	};
+	
+	SetAttr(defAttr);
+	cout<<"    1  2  3  4  5  6  7  8\n";
+	for(int r=0;r<8;r++){
+		SetAttr(defAttr);
+		cout<<" "<<(r+1)<<"  ";
+		for(int c=0;c<8;c++){
+			bool isLight=((r+c)%2==0);
+			WORD bg=isLight?light:dark;
+			
+			if(hasSel && sel.r==r && sel.c==c) bg=Bg(6);
+			else if(isMarked(r,c)) bg=Bg(5);
+			
+			WORD fg=emptyFg;
+			if(b.s[r][c].c==WHITE) fg=wfg;
+			else if(b.s[r][c].c==BLACK) fg=bfg;
+			
+			SetAttr(bg|fg);
+			cout<<b.s[r][c].t<<' ';
+		}
+		SetAttr(defAttr);
+		cout<<"\n";
+	}
+	SetAttr(defAttr);
+	cout<<"轮到："<<(b.sideToMove==WHITE?"白方":"黑方")<<"\n";
+}
+
+static Pos FindKing(const Board& b,Color c){
+	for(int r=0;r<8;r++){
+		for(int c2=0;c2<8;c2++){
+			if(b.s[r][c2].c==c && b.s[r][c2].t=='K') return {r,c2};
+		}
+	}
+	return {-1,-1};
+}
+
+static bool IsSquareAttacked(const Board& b,int r,int c,Color by){
+	for(int i=0;i<8;i++){
+		for(int j=0;j<8;j++){
+			Piece p=b.s[i][j];
+			if(p.c!=by) continue;
+			
+			int dr=r-i,dc=c-j;
+			
+			if(p.t=='P'){
+				int dir=(by==WHITE?+1:-1);
+				if(dr==dir && (dc==1||dc==-1)) return true;
+			}else if(p.t=='N'){
+				int adr=abs(dr),adc=abs(dc);
+				if((adr==2&&adc==1)||(adr==1&&adc==2)) return true;
+			}else if(p.t=='K'){
+				if(max(abs(dr),abs(dc))==1) return true;
+			}else if(p.t=='B'||p.t=='Q'){
+				if(abs(dr)==abs(dc) && dr!=0){
+					int stepr=(dr>0?1:-1),stepc=(dc>0?1:-1);
+					int rr=i+stepr,cc=j+stepc;
+					bool ok=true;
+					while(rr!=r && cc!=c){
+						if(b.s[rr][cc].c!=NONE){ ok=false; break; }
+						rr+=stepr; cc+=stepc;
+					}
+					if(ok) return true;
+				}
+			}
+			if(p.t=='R'||p.t=='Q'){
+				if((dr==0&&dc!=0)||(dc==0&&dr!=0)){
+					int stepr=(dr==0?0:(dr>0?1:-1));
+					int stepc=(dc==0?0:(dc>0?1:-1));
+					int rr=i+stepr,cc=j+stepc;
+					bool ok=true;
+					while(rr!=r || cc!=c){
+						if(b.s[rr][cc].c!=NONE){ ok=false; break; }
+						rr+=stepr; cc+=stepc;
+					}
+					if(ok) return true;
+				}
+			}
+		}
+	}
+	return false;
+}
+
+static bool InCheck(const Board& b,Color who){
+	Pos k=FindKing(b,who);
+	if(k.r==-1) return true;
+	return IsSquareAttacked(b,k.r,k.c,(who==WHITE?BLACK:WHITE));
+}
+
+struct ApplyInfo{
+	bool didCastle=false;
+	bool didEnPassant=false;
+	bool didPromotion=false;
+	bool didCapture=false;
+};
+
+static Board ApplyMove(const Board& b,const Move& m,ApplyInfo* info=nullptr){
+	if(info) *info=ApplyInfo{};
+	Board nb=b;
+	
+	Color us=b.sideToMove;
+	Color them=(us==WHITE?BLACK:WHITE);
+	
+	Piece p=nb.s[m.from.r][m.from.c];
+	Piece captured=nb.s[m.to.r][m.to.c];
+	
+	nb.epAvail=false;
+	nb.epTarget={-1,-1};
+	
+	nb.s[m.from.r][m.from.c]=Make(NONE,'.');
+	
+	if(m.isCastle && p.t=='K'){
+		if(info) info->didCastle=true;
+		
+		nb.s[m.to.r][m.to.c]=p;
+		
+		if(us==WHITE){
+			nb.cr.wk=nb.cr.wq=false;
+			if(m.to.c==6){
+				nb.s[0][5]=nb.s[0][7];
+				nb.s[0][7]=Make(NONE,'.');
+			}else{
+				nb.s[0][3]=nb.s[0][0];
+				nb.s[0][0]=Make(NONE,'.');
+			}
+		}else{
+			nb.cr.bk=nb.cr.bq=false;
+			if(m.to.c==6){
+				nb.s[7][5]=nb.s[7][7];
+				nb.s[7][7]=Make(NONE,'.');
+			}else{
+				nb.s[7][3]=nb.s[7][0];
+				nb.s[7][0]=Make(NONE,'.');
+			}
+		}
+	}else if(m.isEnPassant && p.t=='P'){
+		if(info){ info->didEnPassant=true; info->didCapture=true; }
+		
+		nb.s[m.to.r][m.to.c]=p;
+		int capR=m.to.r+(us==WHITE?-1:+1);
+		nb.s[capR][m.to.c]=Make(NONE,'.');
+	}else{
+		if(captured.c!=NONE && info) info->didCapture=true;
+		
+		nb.s[m.to.r][m.to.c]=p;
+		
+		if(m.promo && p.t=='P'){
+			nb.s[m.to.r][m.to.c].t=m.promo;
+			if(info) info->didPromotion=true;
+		}
+		
+		if(p.t=='P' && abs(m.to.r-m.from.r)==2){
+			nb.epAvail=true;
+			nb.epTarget={(m.from.r+m.to.r)/2,m.from.c};
+		}
+		
+		if(us==WHITE){
+			if(p.t=='K'){ nb.cr.wk=nb.cr.wq=false; }
+			if(p.t=='R'){
+				if(m.from.r==0 && m.from.c==0) nb.cr.wq=false;
+				if(m.from.r==0 && m.from.c==7) nb.cr.wk=false;
+			}
+			if(captured.c==BLACK && captured.t=='R'){
+				if(m.to.r==7 && m.to.c==0) nb.cr.bq=false;
+				if(m.to.r==7 && m.to.c==7) nb.cr.bk=false;
+			}
+		}else{
+			if(p.t=='K'){ nb.cr.bk=nb.cr.bq=false; }
+			if(p.t=='R'){
+				if(m.from.r==7 && m.from.c==0) nb.cr.bq=false;
+				if(m.from.r==7 && m.from.c==7) nb.cr.bk=false;
+			}
+			if(captured.c==WHITE && captured.t=='R'){
+				if(m.to.r==0 && m.to.c==0) nb.cr.wq=false;
+				if(m.to.r==0 && m.to.c==7) nb.cr.wk=false;
+			}
+		}
+	}
+	
+	nb.sideToMove=them;
+	return nb;
+}
+
+static void AddIfLegal(const Board& b,vector<Move>& out,const Move& m){
+	Board nb=ApplyMove(b,m,nullptr);
+	if(!InCheck(nb,b.sideToMove)) out.push_back(m);
+}
+
+static void GenPseudo(const Board& b,vector<Move>& out){
+	Color us=b.sideToMove;
+	Color them=(us==WHITE?BLACK:WHITE);
+	
+	for(int r=0;r<8;r++){
+		for(int c=0;c<8;c++){
+			Piece p=b.s[r][c];
+			if(p.c!=us) continue;
+			
+			if(p.t=='P'){
+				int dir=(us==WHITE?+1:-1);
+				int startRow=(us==WHITE?1:6);
+				int promoRow=(us==WHITE?7:0);
+				int r1=r+dir;
+				
+				if(In(r1,c) && b.s[r1][c].c==NONE){
+					Move m{{r,c},{r1,c},0,false,false};
+					if(r1==promoRow){
+						for(char pr: {'Q','R','B','N'}){ Move pm=m; pm.promo=pr; out.push_back(pm); }
+					}else out.push_back(m);
+					
+					int r2=r+2*dir;
+					if(r==startRow && In(r2,c) && b.s[r2][c].c==NONE){
+						out.push_back({{r,c},{r2,c},0,false,false});
+					}
+				}
+				
+				for(int dc: {-1,+1}){
+					int cc=c+dc;
+					if(!In(r1,cc)) continue;
+					
+					if(b.s[r1][cc].c==them){
+						Move m{{r,c},{r1,cc},0,false,false};
+						if(r1==promoRow){
+							for(char pr: {'Q','R','B','N'}){ Move pm=m; pm.promo=pr; out.push_back(pm); }
+						}else out.push_back(m);
+					}
+					
+					if(b.epAvail && b.epTarget.r==r1 && b.epTarget.c==cc){
+						int capR=r1+(us==WHITE?-1:+1);
+						if(In(capR,cc) && b.s[capR][cc].c==them && b.s[capR][cc].t=='P'){
+							out.push_back({{r,c},{r1,cc},0,false,true});
+						}
+					}
+				}
+			}else if(p.t=='N'){
+				int drs[8]={-2,-2,-1,-1,1,1,2,2};
+				int dcs[8]={-1,1,-2,2,-2,2,-1,1};
+				for(int k=0;k<8;k++){
+					int rr=r+drs[k],cc=c+dcs[k];
+					if(!In(rr,cc)) continue;
+					if(b.s[rr][cc].c!=us) out.push_back({{r,c},{rr,cc},0,false,false});
+				}
+			}else if(p.t=='B'||p.t=='R'||p.t=='Q'){
+				vector<pair<int,int>> dirs;
+				if(p.t=='B'||p.t=='Q'){
+					dirs.push_back({1,1}); dirs.push_back({1,-1});
+					dirs.push_back({-1,1}); dirs.push_back({-1,-1});
+				}
+				if(p.t=='R'||p.t=='Q'){
+					dirs.push_back({1,0}); dirs.push_back({-1,0});
+					dirs.push_back({0,1}); dirs.push_back({0,-1});
+				}
+				for(auto [dr,dc]: dirs){
+					int rr=r+dr,cc=c+dc;
+					while(In(rr,cc)){
+						if(b.s[rr][cc].c==us) break;
+						out.push_back({{r,c},{rr,cc},0,false,false});
+						if(b.s[rr][cc].c==them) break;
+						rr+=dr; cc+=dc;
+					}
+				}
+			}else if(p.t=='K'){
+				for(int dr=-1;dr<=1;dr++){
+					for(int dc=-1;dc<=1;dc++){
+						if(dr==0&&dc==0) continue;
+						int rr=r+dr,cc=c+dc;
+						if(!In(rr,cc)) continue;
+						if(b.s[rr][cc].c!=us) out.push_back({{r,c},{rr,cc},0,false,false});
+					}
+				}
+				
+				if(us==WHITE && r==0 && c==4){
+					if(b.cr.wk && b.s[0][5].c==NONE && b.s[0][6].c==NONE){
+						if(!IsSquareAttacked(b,0,4,them) && !IsSquareAttacked(b,0,5,them) && !IsSquareAttacked(b,0,6,them)){
+							out.push_back({{0,4},{0,6},0,true,false});
+						}
+					}
+					if(b.cr.wq && b.s[0][1].c==NONE && b.s[0][2].c==NONE && b.s[0][3].c==NONE){
+						if(!IsSquareAttacked(b,0,4,them) && !IsSquareAttacked(b,0,3,them) && !IsSquareAttacked(b,0,2,them)){
+							out.push_back({{0,4},{0,2},0,true,false});
+						}
+					}
+				}
+				if(us==BLACK && r==7 && c==4){
+					if(b.cr.bk && b.s[7][5].c==NONE && b.s[7][6].c==NONE){
+						if(!IsSquareAttacked(b,7,4,them) && !IsSquareAttacked(b,7,5,them) && !IsSquareAttacked(b,7,6,them)){
+							out.push_back({{7,4},{7,6},0,true,false});
+						}
+					}
+					if(b.cr.bq && b.s[7][1].c==NONE && b.s[7][2].c==NONE && b.s[7][3].c==NONE){
+						if(!IsSquareAttacked(b,7,4,them) && !IsSquareAttacked(b,7,3,them) && !IsSquareAttacked(b,7,2,them)){
+							out.push_back({{7,4},{7,2},0,true,false});
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+static vector<Move> GenLegal(const Board& b){
+	vector<Move> pseudo;
+	pseudo.reserve(128);
+	GenPseudo(b,pseudo);
+	
+	vector<Move> legal;
+	legal.reserve(pseudo.size());
+	for(const auto& m: pseudo) AddIfLegal(b,legal,m);
+	return legal;
+}
+
+static string Key(const Board& b){
+	string k;
+	k.reserve(1+128+6);
+	k.push_back(char('0'+b.sideToMove));
+	
+	for(int r=0;r<8;r++){
+		for(int c=0;c<8;c++){
+			char col=(b.s[r][c].c==WHITE?'w':b.s[r][c].c==BLACK?'b':'.');
+			k.push_back(col);
+			k.push_back(b.s[r][c].t);
+		}
+	}
+	
+	k.push_back(b.cr.wk?'K':'-');
+	k.push_back(b.cr.wq?'Q':'-');
+	k.push_back(b.cr.bk?'k':'-');
+	k.push_back(b.cr.bq?'q':'-');
+	k.push_back(b.epAvail?char('A'+b.epTarget.r):'-');
+	k.push_back(b.epAvail?char('a'+b.epTarget.c):'-');
+	return k;
+}
+
+static bool ReadMove(Move& mv,Color side){
+	string tok;
+	cout<<"> ";
+	if(!(cin>>tok)) return false;
+	
+	string up=tok;
+	for(char& ch: up) ch=toupper((unsigned char)ch);
+	
+	if(up=="O-O" || up=="0-0"){
+		mv.isCastle=true;
+		mv.isEnPassant=false;
+		mv.promo=0;
+		mv.from=(side==WHITE?Pos{0,4}:Pos{7,4});
+		mv.to=(side==WHITE?Pos{0,6}:Pos{7,6});
+		return true;
+	}
+	if(up=="O-O-O" || up=="0-0-0"){
+		mv.isCastle=true;
+		mv.isEnPassant=false;
+		mv.promo=0;
+		mv.from=(side==WHITE?Pos{0,4}:Pos{7,4});
+		mv.to=(side==WHITE?Pos{0,2}:Pos{7,2});
+		return true;
+	}
+	
+	int x1;
+	try{ x1=stoi(tok); }catch(...){ return false; }
+	int y1,x2,y2;
+	if(!(cin>>y1>>x2>>y2)) return false;
+	if(x1<1||x1>8||y1<1||y1>8||x2<1||x2>8||y2<1||y2>8) return false;
+	
+	mv.from={x1-1,y1-1};
+	mv.to={x2-1,y2-1};
+	mv.isCastle=false;
+	mv.isEnPassant=false;
+	mv.promo=0;
+	return true;
+}
+
+static void EnableMouseInput(){
+	HANDLE hIn=GetStdHandle(STD_INPUT_HANDLE);
+	DWORD mode=0;
+	GetConsoleMode(hIn,&mode);
+	mode |= ENABLE_EXTENDED_FLAGS;
+	mode |= ENABLE_MOUSE_INPUT;
+	mode &= ~ENABLE_QUICK_EDIT_MODE;
+	SetConsoleMode(hIn,mode);
+}
+
+static bool ConsoleCharToBoardCell(SHORT x,SHORT y,Pos& out){
+	const int originX=4;
+	const int originY=1;
+	if(y<originY || y>=originY+8) return false;
+	if(x<originX || x>=originX+8*2) return false;
+	int r=y-originY;
+	int c=(x-originX)/2;
+	if(!In(r,c)) return false;
+	out={r,c};
+	return true;
+}
+
+static vector<Pos> MarkTargets(const vector<Move>& legal,Pos from){
+	vector<Pos> m;
+	for(auto &mv: legal) if(mv.from.r==from.r && mv.from.c==from.c) m.push_back(mv.to);
+	return m;
+}
+
+static bool ReadMoveByClickOrKeyboard(const Board& b,const vector<Move>& legal,bool& hasSel,Pos& sel,Move& mv){
+	HANDLE hIn=GetStdHandle(STD_INPUT_HANDLE);
+	DWORD nEvents=0;
+	GetNumberOfConsoleInputEvents(hIn,&nEvents);
+	
+	if(nEvents==0){
+		hasSel=false;
+		return ReadMove(mv,b.sideToMove);
+	}
+	
+	INPUT_RECORD rec;
+	DWORD nRead=0;
+	
+	while(true){
+		if(!ReadConsoleInput(hIn,&rec,1,&nRead)) return false;
+		
+		if(rec.EventType==MOUSE_EVENT){
+			auto& me=rec.Event.MouseEvent;
+			
+			if(me.dwEventFlags==0 && (me.dwButtonState & FROM_LEFT_1ST_BUTTON_PRESSED)){
+				Pos cell;
+				if(!ConsoleCharToBoardCell(me.dwMousePosition.X,me.dwMousePosition.Y,cell)) continue;
+				
+				if(!hasSel){
+					if(b.s[cell.r][cell.c].c!=b.sideToMove) continue;
+					sel=cell;
+					hasSel=true;
+					return false;
+				}else{
+					mv.from=sel;
+					mv.to=cell;
+					mv.isCastle=false;
+					mv.isEnPassant=false;
+					mv.promo=0;
+					
+					Piece p=b.s[mv.from.r][mv.from.c];
+					if(p.c==b.sideToMove && p.t=='K'){
+						if(b.sideToMove==WHITE && mv.from.r==0 && mv.from.c==4 && mv.to.r==0 && (mv.to.c==6||mv.to.c==2)) mv.isCastle=true;
+						if(b.sideToMove==BLACK && mv.from.r==7 && mv.from.c==4 && mv.to.r==7 && (mv.to.c==6||mv.to.c==2)) mv.isCastle=true;
+					}
+					
+					if(p.c==b.sideToMove && p.t=='P' && b.epAvail && mv.to.r==b.epTarget.r && mv.to.c==b.epTarget.c){
+						if(abs(mv.to.c-mv.from.c)==1 && b.s[mv.to.r][mv.to.c].c==NONE) mv.isEnPassant=true;
+					}
+					
+					hasSel=false;
+					return true;
+				}
+			}
+			
+			if(me.dwEventFlags==0 && (me.dwButtonState & RIGHTMOST_BUTTON_PRESSED)){
+				hasSel=false;
+				return false;
+			}
+		}else if(rec.EventType==KEY_EVENT){
+			if(rec.Event.KeyEvent.bKeyDown){
+				FlushConsoleInputBuffer(hIn);
+				hasSel=false;
+				return ReadMove(mv,b.sideToMove);
+			}
+		}
+	}
+}
+
+int main(){
+	EnableMouseInput();
+	
+	WORD defAttr=GetDefaultAttr();
+	Board b;
+	Init(b);
+	
+	mt19937 rng((uint32_t)chrono::high_resolution_clock::now().time_since_epoch().count());
+	b.sideToMove = (uniform_int_distribution<int>(0,1)(rng)==0 ? WHITE : BLACK);
+	cout<<"随机先手："<<(b.sideToMove==WHITE?"白方":"黑方")<<"\n";
+	Sleep(800);
+	
+	unordered_map<string,int> rep;
+	rep[Key(b)]=1;
+	
+	bool hasSel=false;
+	Pos sel{0,0};
+	
+	while(true){
+		auto legal=GenLegal(b);
+		vector<Pos> marks=hasSel?MarkTargets(legal,sel):vector<Pos>{};
+		
+		system("cls");
+		Draw(b,defAttr,hasSel,sel,marks);
+		
+		if(rep[Key(b)]>=3){
+			cout<<"平局：三次重复局面。\n";
+			break;
+		}
+		
+		bool inCheck=InCheck(b,b.sideToMove);
+		if(legal.empty()){
+			if(inCheck){
+				cout<<(b.sideToMove==WHITE?"白方":"黑方")<<"被将死，"<<(b.sideToMove==WHITE?"黑方":"白方")<<"胜利！\n";
+			}else{
+				cout<<"平局：逼和（无子可走且未被将军）。\n";
+			}
+			break;
+		}
+		
+		if(inCheck) cout<<"提示：当前为将军状态，必须应将。\n";
+		cout<<"鼠标左键：点起点再点终点；右键取消选择。也可键盘输入：x1 y1 x2 y2 或 O-O / O-O-O\n";
+		
+		Move mv;
+		bool got=ReadMoveByClickOrKeyboard(b,legal,hasSel,sel,mv);
+		if(!got){
+			continue;
+		}
+		
+		Piece p=b.s[mv.from.r][mv.from.c];
+		if(!mv.isCastle && p.c==b.sideToMove && p.t=='P'){
+			int promoRow=(b.sideToMove==WHITE?7:0);
+			if(mv.to.r==promoRow){
+				cout<<"兵到达底线，需要升变。输入 Q/R/B/N > ";
+				char pr;
+				if(!(cin>>pr)){
+					cin.clear();
+					cin.ignore(numeric_limits<streamsize>::max(),'\n');
+					continue;
+				}
+				pr=toupper((unsigned char)pr);
+				if(pr!='Q'&&pr!='R'&&pr!='B'&&pr!='N'){
+					cout<<"升变输入不合法，按回车继续...";
+					cin.ignore(numeric_limits<streamsize>::max(),'\n');
+					cin.get();
+					continue;
+				}
+				mv.promo=pr;
+			}
+		}
+		
+		bool ok=false;
+		for(const auto& lm: legal){
+			if(SameMove(lm,mv)){ ok=true; break; }
+		}
+		if(!ok){
+			cout<<"非法走法，按回车继续...";
+			cin.ignore(numeric_limits<streamsize>::max(),'\n');
+			cin.get();
+			continue;
+		}
+		
+		ApplyInfo info;
+		Board nb=ApplyMove(b,mv,&info);
+		
+		bool nextInCheck=InCheck(nb,nb.sideToMove);
+		bool show=false;
+		
+		if(info.didCastle){ cout<<"提示：完成易位。\n"; show=true; }
+		if(info.didEnPassant){ cout<<"提示：吃过路兵成功。\n"; show=true; }
+		if(info.didPromotion){ cout<<"提示：完成升变。\n"; show=true; }
+		if(nextInCheck){ cout<<"提示：将军！\n"; show=true; }
+		
+		b=nb;
+		rep[Key(b)]++;
+		
+		if(show){
+			cout<<"按回车继续...";
+			cin.ignore(numeric_limits<streamsize>::max(),'\n');
+			cin.get();
+		}
+	}
+	
+	SetAttr(defAttr);
+	system("pause");
+	return 0;
+}
